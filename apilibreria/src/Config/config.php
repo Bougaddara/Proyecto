4675b2a40e7952803d0256db298c4b72be136ba1
bougaddara <rachidmohamed072@gmail.com>
2021-06-08 11:05:20 +0200

    subir api

--- apilibreria/src/Config/config.php
+++ apilibreria/src/Config/config.php
@@ -6,7 +6,7 @@ interface config {
     CONST DB_HOST = "localhost";
     CONST DB_PORT = "33060";
     
-    CONST DB_USER = "root";
-    CONST DB_PASSWD = "";
+    CONST DB_USER = "mohamed";
+    CONST DB_PASSWD = "Mohamed1.*";
     CONST DB_CHARSET = "utf8";
 }
